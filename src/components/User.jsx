e7c3b91d6b9a54392a88c11df51f283940f1600a
suad <suadabdullahal@gmail.com>
2021-12-18 15:27:12 +0600

    3nd

---
+++ src/components/User.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const User = () => {
+    // const [profile ,setProffile] = useState(null);
+
+    // useEffect(() => {
+    
+    // })
+  return (
+    <div className="user">
+      <h2>User Details</h2>
+    </div>
+  )
+}
+
+export default User
