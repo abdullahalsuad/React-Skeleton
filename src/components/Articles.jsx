a4714a63bdd6832c1f4d0cf3ad89995170e0f8ed
suad <suadabdullahal@gmail.com>
2021-12-21 01:54:17 +0600

    css added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Skeletons from '../../src/skeletons/Skeletons';
 
 const Articles = () => {
+  const [articles, setArticles] = useState(null);
+
+  // runs automatically after initial render
+  useEffect(() => {
+    setTimeout( async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setArticles(data);
+    }, 5000)
+  }, [])
+
   return (
     <div className="articles">
       <h2>All Articles</h2>
+
+      <Skeletons type="title"/>
+      <Skeletons type="text"/>
+      <Skeletons type="thumbnail"/>
+      <Skeletons type="avatar"/>
+
+
+      {articles && articles.map(article => (
+        <div className="article" key={ article.id }>
+          <h3>{ article.title }</h3>
+          <p>{ article.body }</p>
+        </div>
+      ))}
+
+      {!articles && <div>Loading...</div>}
     </div>
   )
 }
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 const User = () => {
-    // const [profile ,setProffile] = useState(null);
+    const [profile ,setProffile] = useState(null);
 
-    // useEffect(() => {
-    
-    // })
+    useEffect(() => {
+      setTimeout( async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        const data = await res.json();
+        setProffile(data)
+      }, 5000)
+    })
   return (
     <div className="user">
       <h2>User Details</h2>
+
+     
+      {profile && (
+        <div className="profile">
+        <h3>{ profile.username }</h3>
+        <p>{ profile.email }</p>
+        <a href={ profile.website }>{ profile.website }</a>
+      </div>
+      )}
+
+{!profile && <div>Loading...</div>}
     </div>
+
   )
 }
 
